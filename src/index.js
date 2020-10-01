3e20e9dd9f4d382cd5a47bcf8e8c2446db7d1e1b
Thomas-Hartmann <thomas@webtrade.dk>
2020-10-01 11:28:34 +0200

    with async await

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import regeneratorRuntime from "regenerator-runtime";
 
 const tb = document.getElementById('tb');
 const url = 'http://localhost:3333/api/users';
@@ -12,9 +13,25 @@ fetch(url)
     tb.innerHTML = trStr;
 });
 
+document.getElementById('btnId').onclick = () => {
+    const id = document.getElementById('inpId').value;
+    fetch(`${url}/${id}`)
+    .then(res=>fetchWithErrorCheck(res))
+    .then(user=>{
+        const userStr = `${user.name} has email: ${user.email}. ${(user.gender==='female')?'She':'He'} is ${user.age} years old`;
+        document.getElementById('divId').innerHTML = userStr;
+    });
+};
 
 
-
+const getUser = async () => {
+    const id = document.getElementById('inpId').value;
+    const userResponse = await fetch(`${url}/${id}`);
+    const userData = await fetchWithErrorCheck(userResponse);
+    const userStr = `${userData.name} has email: ${userData.email}. ${(userData.gender==='female')?'She':'He'} is ${userData.age} years old`;
+    document.getElementById('divId').innerHTML = userStr;
+}
+document.getElementById('btnId').onclick = getUser;
 
 
 function fetchWithErrorCheck(res){
